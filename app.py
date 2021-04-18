317e107e389fbc5f131e4cba3fd40bc2d4f2f2d0
Prakhar Rathi <38958532+prakharrathi25@users.noreply.github.com>
2021-04-18 17:56:37 +0530

    Initial app setup sent

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import pandas as pd
+import streamlit as st
+
+# Title of the main page
+st.title("Data Storyteller Application")
+
+# Upload the dataset and save as csv
+st.header("Upload files for analysis. These finals should have the same") 
+st.write("\n")
+uploaded_files = st.file_uploader("Upload CSV files", type="csv", accept_multiple_files=True)
+if uploaded_files:
+    for file in uploaded_files:
+        file.seek(0)
+    uploaded_data_read = [pd.read_csv(file) for file in uploaded_files]
+    raw_data = pd.concat(uploaded_data_read)
+
+# Load the data 
+if st.button("Load Data"):
+	
+	# Raw data 
+	st.dataframe(raw_data)
+
+	# Generate a pandas profiling report
+	if st.button("Generate an analysis report"):
+		pass
+
+	# Show the columns in the sidebar 
+	column = st.sidebar.selectbox("Select the column:", raw_data.columns)
+
+	
+
+

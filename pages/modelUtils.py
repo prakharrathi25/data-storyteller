030dd5aed4949ccbfc523201616402b1d6f07c0b
salil <salil>
2021-05-15 21:52:54 +0530

    files moded: modelsutils.py, data_visualize.py

--- pages/modelUtils.py
+++ pages/modelUtils.py
@@ -14,6 +14,7 @@ def makeDir():
 	else: 
 		os.mkdir('../models')
 
+# will save a model at ../models and will return the location+name of saved model
 def saveModel(modelClass, name = None):
 	fileName = name
 	if name is None: 
@@ -22,12 +23,13 @@ def saveModel(modelClass, name = None):
 	pickle.dump(modelClass, open('../models/'+fileName, 'wb'))
 	return '../models/'+fileName
 
+# model will be loaded through the location of model that is returned from the 
 def loadModel(fileName):
 	model = pickle.load(open(fileName, 'rb'))
 	return model
+
 ### All the below tests passed
 if __name__ == '__main__':
-
 	print(checkDir())
 	makeDir()
 	reg = linear_model.Ridge(alpha = 0.5)
